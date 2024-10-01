d0b52320a6d890b4f1acae2c8541222f97c7c86d
박동욱 <wanderhoward@gmail.com>
2024-10-01 11:42:31 +0900

    핵심편 - Chapter 19. 화폐 연산

---
+++ src/core/chapter19/CurrencyOperations.kt
@@ -0,0 +1,39 @@
+package core.chapter19
+
+import java.math.BigDecimal
+
+data class Money(
+    val amount: BigDecimal,
+    val currency: Currency,
+) {
+    operator fun plus(other: Money): Money {
+        return Money(this.amount + other.amount, currency)
+    }
+    operator fun minus(other: Money): Money {
+        return Money(this.amount + other.amount, currency)
+    }
+    operator fun times(scalar: Int): Money {
+        return Money(this.amount * scalar.toBigDecimal(), currency)
+    }
+    operator fun unaryMinus(): Money {
+        return Money(-this.amount, currency)
+    }
+
+    companion object {
+        fun eur(amount: String) = Money(BigDecimal(amount), Currency.EUR)
+    }
+}
+
+enum class Currency {
+    EUR, USD, GBP, RUB
+}
+
+fun main() {
+    val money1 = Money.eur("10.00")
+    val money2 = Money.eur("29.99")
+
+    println(money1 + money2)
+    println(money1 - money2)
+    println(-money1)
+    println(money1 * 3)
+}
