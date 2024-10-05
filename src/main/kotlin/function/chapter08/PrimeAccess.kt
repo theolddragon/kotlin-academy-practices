1d1faaf5690cd3748b5d820c44ead45e919ea700
박동욱 <wanderhoward@gmail.com>
2024-10-05 17:52:50 +0900

    함수편 - Chapter 08. 프라임 접근 리스트

---
+++ src/main/kotlin/function/chapter08/PrimeAccess.kt
@@ -0,0 +1,76 @@
+package function.chapter08
+
+import kotlin.random.Random
+import kotlin.system.measureTimeMillis
+
+class PrimeAccessRepository(
+    private val primeAccessList: PrimeAccessList
+) {
+    fun isOnAllowList(userId: String): Boolean = primeAccessList.entries
+        .any { entry -> entry.userId == userId && entry.allowList }
+    fun isOnDenyList(userId: String): Boolean = primeAccessList.entries
+        .any { entry -> entry.userId == userId && entry.denyList }
+}
+
+class PrimeAccessRepositoryByAssociate(
+    private val primeAccessList: PrimeAccessList
+) {
+    private val entries = primeAccessList.entries.associateBy { it.userId }
+    fun isOnAllowList(userId: String): Boolean = entries[userId]?.allowList ?: false
+    fun isOnDenyList(userId: String): Boolean = entries[userId]?.denyList ?: false
+}
+
+class PrimeAccessList(
+    val entries: List<PrimeAccessEntry>
+)
+
+class PrimeAccessEntry(
+    val userId: String,
+    val allowList: Boolean,
+    val denyList: Boolean,
+)
+
+fun main() {
+    val entries = List(200_000) {
+        PrimeAccessEntry(
+            userId = it.toString(),
+            allowList = Random.nextBoolean(),
+            denyList = Random.nextBoolean()
+        )
+    }.shuffled()
+    val accessList = PrimeAccessList(entries)
+
+    val repo: PrimeAccessRepository
+    measureTimeMillis {
+        repo = PrimeAccessRepository(accessList)
+    }.also { println("Class creation took $it ms") }
+
+    measureTimeMillis {
+        for (userId in 1L..10_000L) {
+            repo.isOnAllowList(userId.toString())
+        }
+    }.also { println("Operation took $it ms") }
+
+    measureTimeMillis {
+        for (userId in 1L..10_000L) {
+            repo.isOnDenyList(userId.toString())
+        }
+    }.also { println("Operation took $it ms") }
+
+    val repoByAssociate: PrimeAccessRepositoryByAssociate
+    measureTimeMillis {
+        repoByAssociate = PrimeAccessRepositoryByAssociate(accessList)
+    }.also { println("Class creation took $it ms") }
+
+    measureTimeMillis {
+        for (userId in 1L..10_000L) {
+            repoByAssociate.isOnAllowList(userId.toString())
+        }
+    }.also { println("Operation took $it ms") }
+
+    measureTimeMillis {
+        for (userId in 1L..10_000L) {
+            repoByAssociate.isOnDenyList(userId.toString())
+        }
+    }.also { println("Operation took $it ms") }
+}
